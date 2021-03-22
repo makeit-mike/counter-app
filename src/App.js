f1f7786e6505ba475b74e047c469006fcd43bbe1
MakeIt-Mike <mike.isaac.jones@gmail.com>
2021-03-22 18:29:28 -0500

    finalizing counter component. ready for reuse

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Components/counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        count
-         
+        <Counter/>
       </header>
     </div>
   );
--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,10 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class  extends Component {
-    state = {  }
-    render() { 
-        return ( 'test' );
+class Counter extends Component {
+  state = {
+    count: 0,
+  }; //the data the component needs
+  render() {
+    return (
+      <div>
+        <label
+          className="itemsInCart"
+          // className="badge badge-primary m-2 "
+        >
+          {this.formatCount()}
+        </label>
+
+        <button
+          style={{ marginRight: 5 + "px" }}
+          className="btn btn-secondary btn-sm "
+          onClick={this.increment}
+        >
+          +
+        </button>
+
+        <button
+          style={{ marginRight: 10 + "px" }}
+          className="btn btn-secondary btn-sm"
+          onClick={this.decrement}
+        >
+          -
+        </button>
+
+        <button
+          style={{ marginRight: 5 + "px" }}
+          className="btn btn-warning btn-sm"
+          onClick={this.delete}
+        >
+          x
+        </button>
+      </div>
+    );
+  }
+  formatCount = () => {
+    return this.state.count == 0 ? "Zero" : this.state.count;
+  };
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decrement = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 });
     }
+  };
+  delete = () => {
+    this.setState({ count: 0 });
+  };
 }
- 
-export default ;
+
+export default Counter;
